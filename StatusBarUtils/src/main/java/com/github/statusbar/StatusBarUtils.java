71ff12049c8a6b8110552388a257a7f370b424ca
zhongrui <zhongrui@021.com>
2020-03-14 03:01:21 +0800

    file

--- StatusBarUtils/src/main/java/com/github/statusbar/StatusBarUtils.java
+++ StatusBarUtils/src/main/java/com/github/statusbar/StatusBarUtils.java
@@ -1,5 +1,16 @@
 package com.github.statusbar;
 
-public class StatusBarUtils {
+import android.app.Activity;
+import android.view.WindowManager;
 
+public class StatusBarUtils {
+    private static boolean isNull(Object object){
+        return object==null;
+    }
+    public static void get(Activity activity){
+        if(isNull(activity)){
+            return;
+        }
+        activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+    }
 }
